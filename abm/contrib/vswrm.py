717b902b09168bb5ee7b690c9db3d8e5919b4747
mezdahun <mezeydavid@gmail.com>
2021-11-08 17:36:20 +0100

    spooky sign problem added as todo

--- abm/contrib/vswrm.py
+++ abm/contrib/vswrm.py
@@ -1,8 +1,9 @@
 """parameters for visdualswarm algorithm"""
 
 GAM = 0.1
-V0 = 3
-ALP0 = 0.15
+V0 = 1
+# todo: why does it work fine with negative sign instead of positive one?
+ALP0 = -0.8
 ALP1 = 0.0025
 ALP2 = 0
 BET0 = 0.05

76b6c387d031e19789036d3b1c7bd5b2740d846e
mezdahun <mezeydavid@gmail.com>
2021-11-09 14:23:16 +0100

    Rebase

--- abm/app.py
+++ abm/app.py
@@ -2,5 +2,5 @@ from abm.simulation.sims import Simulation
 
 
 def start():
-    sim = Simulation(N=4, T=1000, v_field_res=1200, width=1000, height=600, show_vis_field=False)
+    sim = Simulation(N=2, T=1000, v_field_res=1200, width=1000, height=600, show_vis_field=False)
     sim.start()

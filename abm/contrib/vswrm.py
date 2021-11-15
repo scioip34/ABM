5a0a25de4b77a6f87beec29c7f11174952468621
mezdahun <mezeydavid@gmail.com>
2021-11-15 14:33:45 +0100

    Change vswrm params to relocation for now

--- abm/contrib/vswrm.py
+++ abm/contrib/vswrm.py
@@ -3,9 +3,9 @@
 GAM = 0.1
 V0 = 1
 # todo: why does it work fine with negative sign instead of positive one?
-ALP0 = -0.05
-ALP1 = 0.001
+ALP0 = -0.15
+ALP1 = 0.006
 ALP2 = 0
-BET0 = 0.05
-BET1 = 0.001
+BET0 = 0.15
+BET1 = 0.006
 BET2 = 0

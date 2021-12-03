37f75396a4f79377851742241d522abc45be0abf
mezdahun <mezeydavid@gmail.com>
2021-12-03 14:58:54 +0100

    finalized csv save

--- abm/contrib/ifdb_params.py
+++ abm/contrib/ifdb_params.py
@@ -3,3 +3,6 @@ INFLUX_PORT = "8086"
 INFLUX_USER = "monitoring"
 INFLUX_PSWD = "password"
 INFLUX_DB_NAME = "home"
+
+# SAVE_DIR is counted from the ABM parent directory.
+SAVE_DIR = "abm/data/simulation_data"

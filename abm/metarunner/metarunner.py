9194a490efa4456e26af888416d504d60feff417
mezdahun <mezeydavid@gmail.com>
2022-01-18 11:22:21 +0100

    Metaprotocol generates env files according to criterions

--- abm/metarunner/metarunner.py
+++ abm/metarunner/metarunner.py
@@ -4,9 +4,20 @@ metarunner.py: including the main classes and methods to programatically ruzn me
 """
 
 import numpy as np
+import os
+import itertools
 from dotenv import dotenv_values
 
-envconf = dotenv_values(".env")
+envconf = dotenv_values("../.env")
+
+
+def generate_env_file(env_data, file_name, save_folder):
+    """Generating a single env file under save_folder with file_name including env_data as env format"""
+    os.makedirs(save_folder, exist_ok=True)
+    file_path = os.path.join(save_folder, file_name)
+    with open(file_path, "a") as file:
+        for k, v in env_data.items():
+            file.write(f"{k}={v}\n")
 
 
 class Constant:
@@ -15,6 +26,10 @@ class Constant:
     def __init__(self, var_name, constant):
         """defines a single variable value with name"""
         self.tunable = Tunable(var_name, values_override=[constant])
+        self.name = self.tunable.name
+
+    def get_values(self):
+        return self.tunable.values
 
     def print(self):
         """printing method"""
@@ -56,9 +71,13 @@ class Tunable:
         print(f"Tunable: {self.name} = {self.min_val}  -  -  -n={self.n_data}-  -  -  {self.max_val}")
         print(f"Values : {self.values}")
 
+    def get_values(self):
+        return self.values
+
 
 class MetaProtocol:
     """Metaprotocol class that is initialized with Tunables and runs through the desired simulations accordingly"""
+
     def __init__(self):
         self.default_envconf = envconf
         self.tunables = []
@@ -68,3 +87,29 @@ class MetaProtocol:
         self.tunables.append(criterion)
         print("---Added new criterion to MetaProtocol: ")
         criterion.print()
+
+    def generate_temp_env_files(self):
+        """generating a batch of env files that will describe the metaprotocol in a temporary folder"""
+
+        temp_dir = "abm/data/metaprotocol/temp"
+        root_abm_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+        temp_dir = os.path.join(root_abm_dir, temp_dir)
+
+        tunable_names = [t.name for t in self.tunables]
+        tunable_values = [t.get_values() for t in self.tunables]
+        combos = list(itertools.product(*tunable_values))
+
+        print(f"Generating {len(combos)} env files for simulations")
+
+        for i, combo in enumerate(combos):
+            new_envconf = self.default_envconf.copy()
+            for j, value in enumerate(combo):
+                name = tunable_names[j]
+                if not isinstance(value, bool):
+                    new_envconf[name] = value
+                else:
+                    new_envconf[name] = int(value)
+            generate_env_file(new_envconf, f"{i}.env", temp_dir)
+
+        print(f"Env files generated according to criterions!")
+

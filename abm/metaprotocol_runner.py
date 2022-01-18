f3e1f7b67df318206cabb5f1c8b30b23a428a930
mezdahun <mezeydavid@gmail.com>
2022-01-18 12:38:38 +0100

    Added example metaprotocol runner

---
+++ abm/metaprotocol_runner.py
@@ -0,0 +1,19 @@
+from abm.metarunner.metarunner import Tunable, Constant, MetaProtocol
+
+# Defining criterions as parameter ranges (Tunables) and single parameter values (Constants)
+criteria = []
+criteria.append(Tunable("ENV_WIDTH", 600, 800, 3))
+criteria.append(Tunable("ENV_HEIGHT", 700, 800, 2))
+criteria.append(Constant("T", 100))
+
+# Creating metaprotocol and add defined criteria
+mp = MetaProtocol()
+for crit in criteria:
+    mp.add_criterion(crit)
+
+# Generating temporary env files with criterion combinations. Comment this out if you want to contzinue simulating due
+# to interruption
+mp.generate_temp_env_files()
+
+# Running the simulations
+mp.run_protocols()

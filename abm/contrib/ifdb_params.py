c12fc2126b125b019a8111a287535f4bdcbe7bba
mezdahun <mezeydavid@gmail.com>
2022-01-24 10:00:50 +0100

    saving directory
    can be controlled with env file

--- abm/contrib/ifdb_params.py
+++ abm/contrib/ifdb_params.py
@@ -1,5 +1,10 @@
 import os
 import datetime
+from dotenv import dotenv_values
+
+root_abm_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+env_path = os.path.join(root_abm_dir, ".env")
+envconf = dotenv_values(env_path)
 
 INFLUX_HOST = "127.0.0.1"
 INFLUX_PORT = "8086"
@@ -8,7 +13,7 @@ INFLUX_PSWD = "password"
 INFLUX_DB_NAME = "home"
 
 # SAVE_DIR is counted from the ABM parent directory.
-SAVE_DIR = "abm/data/simulation_data"
+SAVE_DIR = envconf.get("SAVE_ROOT_DIR", "abm/data/simulation_data")
 
 # create base folder in data
 root_abm_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))

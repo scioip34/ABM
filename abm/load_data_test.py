03b0008115601c59e5da9a122f2b162eb3a15968
mezdahun <mezeydavid@gmail.com>
2022-01-26 10:30:26 +0100

    Change example of ExperimentLoader usage

--- abm/load_data_test.py
+++ abm/load_data_test.py
@@ -1,3 +1,4 @@
 from abm.loader.data_loader import ExperimentLoader
 
-loaded_experiment = ExperimentLoader("/home/david/Desktop/ABM/abm/data/simulation_data/StandardData3vars")
+loaded_experiment = ExperimentLoader("/home/david/Desktop/ABM/abm/data/simulation_data/Experiment1_3agents",
+                                     enforce_summary=False)

855d0311c78edf309fb6b1bda6ede33e1f30e082
mezdahun <mezeydavid@gmail.com>
2022-02-14 11:31:10 +0100

    introuce undersample during csv read

--- abm/loader/helper.py
+++ abm/loader/helper.py
@@ -10,16 +10,30 @@ def load_csv_file(path):
         reader = csv.reader(infile)
         new_dict = {}
         dict_keys = []
-        for ri, row in enumerate(reader):
-            if ri==0:
+        main_iter = iter(enumerate(reader))
+        # for ri, row in enumerate(reader):
+        #     if ri == 0:
+        #         dict_keys = list(row)
+        #         dict_keys = ["t" if x == "" else x for x in dict_keys]
+        #         for key in dict_keys:
+        #             new_dict[key] = []
+        #     else:
+        #         if ri % undersample == 0:
+        #             # print("ADD ROW", ri)
+        #             for ci, key in enumerate(dict_keys):
+        #                 new_dict[key].append(row[ci])
+        for ri, row in main_iter:
+            if ri == 0:
                 dict_keys = list(row)
-                dict_keys = ["t" if x=="" else x for x in dict_keys]
+                dict_keys = ["t" if x == "" else x for x in dict_keys]
                 for key in dict_keys:
                     new_dict[key] = []
             else:
                 for ci, key in enumerate(dict_keys):
                     new_dict[key].append(row[ci])
+                [next(main_iter, None) for _ in range(undersample)]  # skip 5
         return new_dict
+
 
 def reconstruct_VPF(VPF_resolution, up_edge_list, down_edge_list):
     """Constructing a binary VPF array (1/0) according to saved edge data.

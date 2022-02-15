c3e517b1baa842e2deaf05381d41189996173632
mezdahun <mezeydavid@gmail.com>
2022-02-15 13:08:17 +0100

    undersampling is not a must

--- abm/loader/data_loader.py
+++ abm/loader/data_loader.py
@@ -375,7 +375,7 @@ class ExperimentLoader:
         with open(os.path.join(self.experiment_path, "summary", "fixed_env.json"), "r") as fixf:
             self.env = json.loads(fixf.read())
         print("Overwriting undersample ratio with the one read from env file...")
-        self.undersample = int(float(self.env["SUMMARY_UNDERSAMPLE"]))
+        self.undersample = int(float(self.env.get("SUMMARY_UNDERSAMPLE", "1")))
         with open(os.path.join(self.experiment_path, "summary", "tuned_env.json"), "r") as tunedf:
             self.varying_params = json.loads(tunedf.read())
         print("Agent, resource and parameter data reloaded!")

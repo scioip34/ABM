f8c32c4e1d8aeca5cb862f6e4c37175f0be9859f
mezdahun <mezeydavid@gmail.com>
2022-02-18 10:12:27 +0100

    writing batch size chosen according to simulation time

--- abm/contrib/ifdb_params.py
+++ abm/contrib/ifdb_params.py
@@ -13,7 +13,14 @@ INFLUX_PORT = "8086"
 INFLUX_USER = "monitoring"
 INFLUX_PSWD = "password"
 INFLUX_DB_NAME = "home"
-write_batch_size = 1000
+
+T = float(int(envconf["T"]))
+if T <= 1000:
+    write_batch_size = T
+else:
+    if T % 1000 != 0:
+        raise Exception("Simulation time (T) must be dividable by 1000 or smaller than 1000!")
+    write_batch_size = 1000
 
 # SAVE_DIR is counted from the ABM parent directory.
 SAVE_DIR = envconf.get("SAVE_ROOT_DIR", "abm/data/simulation_data")

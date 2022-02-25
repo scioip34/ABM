3f19a4634bdb884b9ea7611c663e3b2a132aab68
mezdahun <mezeydavid@gmail.com>
2022-02-25 10:19:47 +0100

    add requirement, bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='P34 ABM',
     description='Agent based model framework to simulate collectively foraging agents relying on their private and social'
                 'visual cues. Written in pygame and python 3.7+',
-    version='1.1.1',
+    version='1.1.2',
     url='https://github.com/scioip34/ABM',
     maintainer='David Mezey and Dominik Deffner @ SCIoI',
     packages=find_packages(exclude=['tests']),
@@ -21,7 +21,8 @@ setup(
         'python-dotenv',
         'pandas',
         'influxdb',
-        'opencv-python'
+        'opencv-python',
+        'xvfbwrapper'
     ],
     extras_require={
         'test': [

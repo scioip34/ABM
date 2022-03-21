e0ddfe7f3391b50017feca0bcf68e88707b22b85
mezdahun <mezeydavid@gmail.com>
2022-03-21 10:53:18 +0100

    fix exp files by creating temporary experiment files

--- HPC_run_all_exp_distribute.sh
+++ HPC_run_all_exp_distribute.sh
@@ -51,6 +51,9 @@ do
       echo "Env file already exists for experiment $exp_name_hashed"
     fi
 
+    # Preparing temporary exp file with hash
+    cp $search_dir/$exp_name.py $search_dir/$exp_name_hashed.py
+
     # Run an experiment on a dedicated node
     echo "Starting experiment $exp_name_hashed"
     sbatch --export=EXPERIMENT_NAME=$exp_name_hashed ./HPC_batch_run.sh
@@ -58,5 +61,7 @@ do
     # Cleaning up
     # remove env file
     rm ./$exp_name_hashed.env
+    # remove temp exp file
+    rm $search_dir/$exp_name_hashed.py
   done
 done

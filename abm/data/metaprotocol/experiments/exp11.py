5ccec982c76b9641c777fb5244ce5f1dfed8b1eb
mezdahun <mezeydavid@gmail.com>
2022-03-24 15:08:00 +0100

    prepared next experiment

--- abm/data/metaprotocol/experiments/exp11.py
+++ abm/data/metaprotocol/experiments/exp11.py
@@ -64,7 +64,7 @@ arena_size = arena_w * arena_h
 overall_res_area = int(arena_size * 0.2)
 num_patches = [1, 3, 5, 10, 30, 50, 100]
 criteria_exp = [
-    Constant("N", 10),
+    Constant("N", 50),
     Constant("VISUAL_EXCLUSION", 1),
     Constant("AGENT_FOV", 0.5),  # unlimited
     Tunable("DEC_EPSW", values_override=[0, 0.5, 0.75, 1, 2, 3]),

4d2da5a62d4262eac6ec0a8ef5c3ae0f1f9a81f1
mezdahun <mezeydavid@gmail.com>
2022-05-17 11:16:19 +0200

    Added new experiments removed old ones.

--- abm/data/metaprotocol/experiments/humanexp6.py
+++ abm/data/metaprotocol/experiments/humanexp6.py
@@ -9,16 +9,15 @@ description_text = f"""
 Experiment file using the MetaRunner interfacing language to define a set of criteria for batch simulations
 
 Title:      Experiment : {EXP_NAME}
-Date:       16.05.2022
-Goal:       humanexp4: we continue matching parameters with those planned in human experimentation, but now we keep the
-            discoverable area fixed and decrease the arena size by 20 percent. To keep the patch sizes as in previous
-            experiments we increase the discoverable area from 10 percent of the arena to 15.625 percent.
+Date:       13.05.2022
+Goal:       humanexp6: We repeat humanexp1 but with more different patch numbers to see a gradual change in the
+            efficiency plot.
 Defined by: mezdahun
 """
 
 # Defining fixed criteria for all automized simulations/experiments
-arena_w = 400
-arena_h = 400
+arena_w = 500
+arena_h = 500
 fixed_criteria = [
     Constant("USE_IFDB_LOGGING", 1),
     Constant("USE_RAM_LOGGING", 1),  # as we have plenty of resources we don't have to deal with IFDB on HPC
@@ -64,17 +63,16 @@ fixed_criteria = [
 # Defining decision param
 sum_resources = 240
 arena_size = arena_w * arena_h
-overall_res_area = int(arena_size * 0.15625)
-num_patches = [2, 5, 10, 20, 40]
+num_patches = [1, 2, 3, 4, 5, 8, 10, 20, 40]
 criteria_exp = [
     Constant("N", 4),
     Constant("VISUAL_EXCLUSION", 1),  # no visual occlusion
     Constant("AGENT_FOV", 0.21),  # unlimited
-    Tunable("DEC_EPSW", values_override=[0, 5, 10, 25, 50]),
+    Tunable("DEC_EPSW", values_override=[0, 5, 10, 25, 50, 100]),
     Constant("DEC_EPSU", 1),
     Constant("MIN_RESOURCE_QUALITY", 0.25),  # we fix the max quality to negative so can control the value with MIN
     Tunable("MIN_RESOURCE_PER_PATCH", values_override=[int(sum_resources/nup) for nup in num_patches]),  #same here
-    Tunable("RADIUS_RESOURCE", values_override=[np.sqrt(overall_res_area/(np.pi*nup)) for nup in num_patches]),
+    Constant("RADIUS_RESOURCE", 15),
     Constant("DEC_SWU", 0),  # no cross-inhibition
     Constant("DEC_SUW", 0),  # no cross-inhibition
     Tunable("N_RESOURCES", values_override=num_patches),
@@ -92,10 +90,6 @@ for crit in criteria_exp:
 # Locking the overall resource units in environment
 constant_runits = TunedPairRestrain("N_RESOURCES", "MIN_RESOURCE_PER_PATCH", sum_resources)
 mp.add_tuned_pair(constant_runits)
-
-# keeping sicoverable area fixed
-constant_r_area = TunedPairRestrain("N_RESOURCES", "RADIUS_RESOURCE", overall_res_area/np.pi)
-mp.add_quadratic_tuned_pair(constant_r_area)
 
 # Generating temporary env files with criterion combinations. Comment this out if you want to continue simulating due
 # to interruption

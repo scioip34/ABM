763cd1c9a6528d6df43fc497b8d278db084ea37e
vagechirkov <vagechirkov@gmail.com>
2022-11-03 12:05:27 +0100

    isolate the playground startup function for the cooperative signaling

---
+++ abm/projects/cooperative_signaling/app.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+import shutil
+
+from abm.simulation.isims import PlaygroundSimulation
+import abm.contrib.playgroundtool as pgt
+
+from abm.app import save_isims_env
+
+import os
+# loading env variables from dotenv file
+from dotenv import dotenv_values
+
+EXP_NAME = os.getenv("EXPERIMENT_NAME", "cooperative_signaling")
+EXP_NAME_COPY = f"{EXP_NAME}_copy"
+os.path.dirname(os.path.realpath(__file__))
+# root_abm_dir = Path(__file__).parent.parent.parent.parent
+env_file_dir = Path(__file__).parent
+env_path = env_file_dir / f"{EXP_NAME}.env"
+env_path_copy = env_file_dir / f"{EXP_NAME_COPY}.env"
+# make a duplicate of the env file to be used by the playground
+shutil.copyfile(env_path, env_path_copy)
+envconf = dotenv_values(env_path)
+
+
+def start_playground():
+    # changing env file according to playground default parameters before
+    # running any component of the SW
+    save_isims_env(env_file_dir, EXP_NAME_COPY, pgt, envconf)
+    # Start interactive simulation
+    sim = PlaygroundSimulation()
+    sim.start()
+
+
+if __name__ == "__main__":
+    start_playground()
+

87d941f0bd540a9af1eb7fbc3079a14ba34f592c
vagechirkov <vagechirkov@gmail.com>
2022-11-07 17:20:05 +0100

    add tests for the phototaxis algorithm

--- abm/projects/cooperative_signaling/cs_agent/tests/test_cs_supcalc.py
+++ abm/projects/cooperative_signaling/cs_agent/tests/test_cs_supcalc.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 
 from abm.projects.cooperative_signaling.cs_agent import cs_supcalc
 
@@ -12,3 +13,32 @@ def test_random_walk():
 
     assert dvel == 1.0
     assert dtheta == -0.0752759286915825
+
+
+@pytest.mark.parametrize(
+    "meter, prev_meter, prev_theta, taxis_dir, new_theta",
+    [
+        # no change in meter
+        (1, 1, 0, None, 0),
+        # previous meter was larger, and prev_theta > 0
+        (0, 1, 1, None, -1),
+        # previous meter was larger, and prev_theta < 0
+        (0, 1, -1, None, 1),
+        # previous meter was smaller, and prev_theta != 0
+        (1, 0, 1, None, 0),
+        # previous meter was smaller, and prev_theta != 0
+        (1, 0, -1, None, 0),
+        # previous meter was smaller, and prev_theta != 0
+        (0.1, 0, 1, None, 0.9),
+        # previous meter was smaller, and prev_theta != 0
+        (0.1, 0, -1, None, -0.9),
+    ]
+)
+def test_phototaxis(meter, prev_meter, prev_theta, taxis_dir, new_theta):
+    """Test phototaxis()"""
+    phototaxis_theta_step = 1
+
+    theta = cs_supcalc.phototaxis(
+        meter, prev_meter, prev_theta, taxis_dir, phototaxis_theta_step)
+
+    assert new_theta == theta

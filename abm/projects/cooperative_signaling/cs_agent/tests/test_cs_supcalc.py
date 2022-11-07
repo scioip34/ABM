ac9e493024ee64b73807c09c9430f27b3900fa3a
vagechirkov <vagechirkov@gmail.com>
2022-11-07 13:18:43 +0100

    add reflection_from_circular_wall function and tests

--- abm/projects/cooperative_signaling/cs_agent/tests/test_cs_supcalc.py
+++ abm/projects/cooperative_signaling/cs_agent/tests/test_cs_supcalc.py
@@ -12,3 +12,24 @@ def test_random_walk():
 
     assert dvel == 1.0
     assert dtheta == -0.0752759286915825
+
+
+def test_reflection_from_circular_wall():
+    """Test reflection_from_circular_wall()"""
+
+    new_orientation = cs_supcalc.reflection_from_circular_wall(
+        0, 1, np.pi / 2)
+    assert new_orientation == np.pi * 3 / 2
+
+    new_orientation = cs_supcalc.reflection_from_circular_wall(
+        1, 0, np.pi)
+    assert new_orientation == np.pi * 2
+
+    # test very flat reflection angle
+    orient = np.pi + np.pi / 6
+    vec_i = [np.cos(orient), np.sin(orient)]
+    # orientation inside the circle
+    i_orientation = np.pi + np.arctan2(vec_i[1], vec_i[0])
+    new_orientation = cs_supcalc.reflection_from_circular_wall(
+        0, 1, orient)
+    assert new_orientation == i_orientation

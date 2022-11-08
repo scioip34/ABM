36e85df268e3a28aa6cd2c4508685a802c2d1426
mezdahun <mezeydavid@gmail.com>
2022-11-08 13:45:56 +0100

    Commenting algorithm, fixing direction switch and return values

--- abm/projects/cooperative_signaling/cs_agent/cs_supcalc.py
+++ abm/projects/cooperative_signaling/cs_agent/cs_supcalc.py
@@ -45,23 +45,50 @@ def phototaxis(meter, prev_meter, prev_theta, taxis_dir,
     :param meter: current meter (detector) value between 0 and 1
     :param prev_meter: meter value in previous timestep between 0 and 1
     :param prev_theta: turning angle in previous timestep
-    :param taxis_dir: phototaxis direction [-1, 1, None]
+    :param taxis_dir: extended phototaxis direction [-1, 1, None].
+                      None: calculate turning direction according to meter value
+                      [-1, 1]: calculate turning direction w.r.t. direction change
+                               when meter value first dropped
     :param phototaxis_theta_step: maximum turning angle during phototaxis
+    :return new_theta: directional change in orientation according to phototaxis
+    :return new_taxis_dir: extended new phototaxis direction [-1, 1, None].
+                      None: calculate turning direction according to meter value
+                      [-1, 1]: calculate turning direction w.r.t. direction change
+                               when meter value first dropped
     """
+    # calculating difference between current and previous meter values
     diff = meter - prev_meter
+
+    # ------ CALCULATING DIRECTION OF ROTATION ------ #
+    # sign of difference gives information about getting closer or farther
+    # from resource patch.
     sign_diff = np.sign(diff)
-    # positive means the given change in orientation was correct
-    # zero means we are moving in the right direction
+
+    # positive means the given change in orientation increased the meter value.
     if sign_diff >= 0:
         new_sign = np.sign(prev_theta) if prev_theta != 0 else 1
-    # negative means we need to turn the other direction
+        # calculate turning direction according to meter value in next step as well
+        new_taxis_dir = None
+
+    # negative means we are moving in the wrong direction and need to turn the other direction
     else:
+        # so far the meter values were growing, no direction switch has been yet initiated
         if taxis_dir is None:
-            # new sign is a flipped sign of the previous theta
-            new_sign = sign_diff * np.sign(prev_theta)
+            # initiating direction switch and turning to that direction until meter
+            # values will grow again
+            new_taxis_dir = sign_diff * np.sign(prev_theta)
+            new_sign = new_taxis_dir
+
+        # we already initiated a direction switch before, so we keep acting accordingly
         else:
-            new_sign = taxis_dir
-    # change theta proportionally to the meter values
-    # the larger the meter value, the smaller the turning angle
-    new_theta = phototaxis_theta_step * new_sign * (1 - meter)
-    return new_theta
+            # we keep moving in the previously set phototaxis direction
+            new_taxis_dir = taxis_dir
+            new_sign = new_taxis_dir
+
+    # ------ CALCULATING EXTENT OF ROTATION ------ #
+    # change theta proportionally to the meter values,
+    # the larger the meter value, the closer the agent to the center.
+    # As agents are forced to move continuously, closer to the center
+    # agents need to turn faster to stay close to the center.
+    new_theta = phototaxis_theta_step * new_sign * meter
+    return new_theta, new_taxis_dir

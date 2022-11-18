f4cee6ed07eb445eb47648d91f4fc39c2746be81
vagechirkov <vagechirkov@gmail.com>
2022-11-18 18:16:56 +0100

    clean up the simulation test

--- abm/simulation/tests/test_simulation.py
+++ abm/simulation/tests/test_simulation.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-from abm.parameters.agent_parameters import AgentParameters
 from abm.parameters.playground_parameters import PlaygroundParameters
 from abm.simulation.sims import Simulation
 
@@ -11,13 +10,10 @@ def test_init_simulation():
     """
     test_env_file = Path(__file__).parent / 'data' / 'test.env'
 
-    agent_params = AgentParameters(_env_file=test_env_file)
     playground_params = PlaygroundParameters(_env_file=test_env_file)
 
     # merge all parameters into one dict
     kwargs = {
-        # **agent_params.agent_movement.dict(),
-        # **agent_params.agent_decision.dict(),
         **playground_params.environment.dict(),
         **playground_params.ui.dict(),
         **playground_params.agent.dict(),

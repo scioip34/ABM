64cd0343a5e7c241875d7c75a6810e67dc6d4f35
vagechirkov <vagechirkov@gmail.com>
2022-11-18 18:15:04 +0100

    update playground params

--- abm/parameters/playground_parameters.py
+++ abm/parameters/playground_parameters.py
@@ -1,8 +1,9 @@
-from pydantic import BaseSettings
+from pydantic import BaseSettings, BaseModel
 
 
-class PlaygroundEnvironmentParameters(BaseSettings):
+class PlaygroundEnvironmentParameters(BaseModel):
     """Environment variables"""
+    t: int = 1000
     width: int = 500
     height: int = 500
     framerate: int = 30  # interactive
@@ -11,10 +12,9 @@ class PlaygroundEnvironmentParameters(BaseSettings):
     use_zarr: bool = True
     save_csv_files: bool = False
     parallel: bool = False
-    allow_border_patch_overlap: bool = True
 
 
-class PlaygroundUIParameters(BaseSettings):
+class PlaygroundUIParameters(BaseModel):
     """Playground parameters"""
     with_visualization: bool = True
     show_vis_field: bool = False
@@ -22,10 +22,11 @@ class PlaygroundUIParameters(BaseSettings):
     show_vision_range: bool = True
 
 
-class PlaygroundAgentParameters(BaseSettings):
+class PlaygroundAgentParameters(BaseModel):
     """Agent variables"""
+    n: int = 3
     pooling_time: int = 0
-    pooling_prob: int = 0
+    pooling_prob: float = 0
     agent_radius: int = 10
     agent_consumption: int = 1
     vision_range: int = 2000
@@ -36,7 +37,7 @@ class PlaygroundAgentParameters(BaseSettings):
     collide_agents: bool = False
 
 
-class PlaygroundResourceParameters(BaseSettings):
+class PlaygroundResourceParameters(BaseModel):
     """Resource variables"""
     min_resc_perpatch: int = 200
     max_resc_perpatch: int = 201
@@ -45,13 +46,15 @@ class PlaygroundResourceParameters(BaseSettings):
     patch_radius: int = 15  # interactive
     regenerate_patches: bool = True
     teleport_exploit: bool = False
+    allow_border_patch_overlap: bool = True
 
 
 class PlaygroundParameters(BaseSettings):
-    environment_parameters: PlaygroundEnvironmentParameters = \
-        PlaygroundEnvironmentParameters()
-    ui_parameters: PlaygroundUIParameters = PlaygroundUIParameters()
-    agent_parameters: PlaygroundAgentParameters = \
-        PlaygroundAgentParameters()
-    resource_parameters: PlaygroundResourceParameters = \
-        PlaygroundResourceParameters()
+    environment: PlaygroundEnvironmentParameters
+    ui: PlaygroundUIParameters
+    agent: PlaygroundAgentParameters
+    resource: PlaygroundResourceParameters
+
+    class Config:
+        env_file = '.env'
+        env_nested_delimiter = '__'

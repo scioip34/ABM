efde01e4ce0faec0301389a2b276571be4baaca9
mezdahun <mezeydavid@gmail.com>
2022-12-13 15:26:17 +0100

    parametrize crowding via env

---
+++ abm/projects/cooperative_signaling/cs_contrib/cs_signaling_params.py
@@ -0,0 +1,14 @@
+# loading env variables from dotenv file
+from dotenv import dotenv_values
+import os
+
+EXP_NAME = os.getenv("EXPERIMENT_NAME", "")
+
+root_abm_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(
+    os.path.dirname(os.path.realpath(__file__))))))
+env_path = os.path.join(root_abm_dir, f"{EXP_NAME}.env")
+envconf = dotenv_values(env_path)
+
+# Crowding parameters
+max_proj_size_percentage = float(envconf.get("MAX_PROJ_SIZE_PERCENTAGE", 0.5))
+crowd_density_threshold = float(envconf.get("CROWD_DENSITY_THRESHOLD", 0.45))

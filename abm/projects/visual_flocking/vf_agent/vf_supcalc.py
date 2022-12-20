518fa3123bbe9c64fe44e3373b64f835be22af84
mezdahun <mezeydavid@gmail.com>
2022-12-20 16:03:56 +0100

    first functional version of visual flocking project

---
+++ abm/projects/visual_flocking/vf_agent/vf_supcalc.py
@@ -0,0 +1,193 @@
+import numpy as np
+
+from abm.agent.supcalc import angle_between, find_nearest
+from abm.projects.visual_flocking.vf_contrib import vf_params
+
+from scipy import integrate
+
+
+def projection_field(fov, v_field_resolution, position, radius,
+                     orientation, object_positions):
+    """
+    Calculating visual projection field for the agent given the visible
+    obstacles in the environment
+    obstacle sprites to generate projection field
+    :param fov: tuple of number with borders of fov such as (-np.pi, np.pi)
+    :param v_field_resolution: visual field resolution in pixels
+    :param position: np.xarray of agent's position
+    :param radius: radius of the agent
+    :param orientation: orientation angle between 0 and 2pi
+    :param max_proj_size: maximum projection size to include in the visual proj. field
+    :return: projection field np.xarray with shape (n objects, field resolution)
+    """
+    # initializing visual field and relative angles
+    v_field = np.zeros((len(object_positions), v_field_resolution))
+    phis = np.linspace(-np.pi, np.pi, v_field_resolution)
+
+    # center point
+    agents_center = position + radius
+
+    # point on agent's edge circle according to it's orientation
+    agent_edge = position + np.array(
+        [1 + np.cos(orientation), 1 - np.sin(orientation)]) * radius
+
+    # vector between center and edge according to orientation
+    v1 = agent_edge - agents_center
+
+    # 1. Calculating closed angle between object and agent according to the
+    # position of the object.
+    # 2. Calculating visual projection size according to visual angle on
+    # the agent's retina according to distance between agent and object
+    for i, obj_position in enumerate(object_positions):
+        # continue if the object and the agent position completely coincide
+        if obj_position[0] == position[0] and obj_position[1] == position[1]:
+            continue
+
+        # center of obstacle (as it must be another agent)
+        object_center = obj_position + radius
+
+        # vector between agent center and object center
+        v2 = object_center - agents_center
+
+        # calculating closed angle between v1 and v2
+        closed_angle = calculate_closed_angle(v1, v2)
+
+        distance = np.linalg.norm(object_center - agents_center)
+
+        # calculating the visual angle from focal agent to target
+        vis_angle = 2 * np.arctan(radius / (1 * distance))
+
+        # finding where in the retina the projection belongs to
+        phi_target = find_nearest(phis, closed_angle)
+
+        # if target is visible we save its projection into the VPF
+        # source data
+        if fov[0] <= closed_angle <= fov[1]:
+            # the projection size is proportional to the visual angle.
+            # If the projection is maximal (i.e. taking each pixel of the
+            # retina) the angle is 2pi from this we just calculate the
+            # projection size using a single proportion
+            proj_size = (vis_angle / (2 * np.pi)) * v_field_resolution
+
+            proj_start = int(phi_target - np.floor(proj_size / 2))
+            proj_end = int(phi_target + np.floor(proj_size / 2))
+
+            # circular boundaries to the VPF as there is 360 degree vision
+            if proj_start < 0:
+                v_field[i, v_field_resolution + proj_start:v_field_resolution] = 1
+                proj_start = 0
+            if proj_end >= v_field_resolution:
+                v_field[i, 0:proj_end - (v_field_resolution - 1)] = 1
+                proj_end = v_field_resolution
+
+            v_field[i, proj_start:proj_end] = 1
+
+
+    # post_processing and limiting FOV
+    # flip field data along second dimension
+    v_field_post = np.flip(v_field, axis=1)
+
+    v_field_post[:, phis < fov[0]] = 0
+    v_field_post[:, phis > fov[1]] = 0
+    return v_field_post
+
+
+
+def calculate_closed_angle(v1, v2):
+    """
+    Calculating closed angle between two vectors v1 and v2. Rotated with the orientation of the agent as it is relative.
+    :param v1: vector 1; np.xarray
+    :param v2: vector 2; np.xarray
+    :return: closed angle between v1 and v2
+    """
+    closed_angle = angle_between(v1, v2)
+    closed_angle = (closed_angle % (2 * np.pi))
+    # at this point closed angle between 0 and 2pi, but we need it between -pi and pi
+    # we also need to take our orientation convention into consideration to
+    # recalculate theta=0 is pointing to the right
+    if 0 <= closed_angle <= np.pi:
+        closed_angle = -closed_angle
+    else:
+        closed_angle = 2 * np.pi - closed_angle
+    return closed_angle
+
+
+# Functions needed for VSWRM functionality
+def VSWRM_flocking_state_variables(vel_now, Phi, V_now, t_now=None, V_prev=None, t_prev=None):
+    """Calculating state variables of a given agent according to the main algorithm as in
+    https://advances.sciencemag.org/content/6/6/eaay0792.
+        Args:
+            vel_now: current speed of the agent
+            V_now: current binary visual projection field array
+            Phi: linspace numpy array of visual field axis
+            t_now: current time
+            V_prev: previous binary visual projection field array
+            t_prev: previous time
+        Returns:
+            dvel: temporal change in agent velocity
+            dpsi: temporal change in agent heading angle
+
+    """
+    # # Deriving over t
+    # if V_prev is not None and t_prev is not None and t_now is not None:
+    #     dt = t_now - t_prev
+    #     logger.debug('Movement calculation called with NONE as time-related parameters.')
+    #     joined_V = np.vstack((V_prev, t_prev))
+    #     dt_V = dt_V_of(dt, joined_V)
+    # else:
+    #     dt_V = np.zeros(len(Phi))
+
+    dt_V = np.zeros(len(Phi))
+
+    # Deriving over Phi
+    dPhi_V = dPhi_V_of(Phi, V_now)
+
+    # Calculating series expansion of functional G
+    G_vel = (-V_now + vf_params.ALP2 * dt_V)
+
+    # Spikey parts shall be handled separately because of numerical integration
+    G_vel_spike = np.square(dPhi_V)
+
+    G_psi = (-V_now + vf_params.BET2 * dt_V)
+
+    # Spikey parts shall be handled separately because of numerical integration
+    G_psi_spike = np.square(dPhi_V)
+
+    # Calculating change in velocity and heading direction
+    dPhi = Phi[-1] - Phi[-2]
+    FOV_rescaling_cos = 1
+    FOV_rescaling_sin = 1
+
+    # print(f"alp0 : {vswrm.ALP0 * integrate.trapz(np.cos(FOV_rescaling_cos * Phi) * G_vel, Phi)}", )
+    # print(f'alp1 : {vswrm.ALP0 * vswrm.ALP1 * np.sum(np.cos(Phi) * G_vel_spike) * dPhi}')
+
+    dvel = vf_params.GAM * (vf_params.V0 - vel_now) + \
+           vf_params.ALP0 * integrate.trapz(np.cos(FOV_rescaling_cos * Phi) * G_vel, Phi) + \
+           vf_params.ALP0 * vf_params.ALP1 * np.sum(np.cos(Phi) * G_vel_spike) * dPhi
+    dpsi = vf_params.BET0 * integrate.trapz(np.sin(Phi) * G_psi, Phi) + \
+           vf_params.BET0 * vf_params.BET1 * np.sum(np.sin(FOV_rescaling_sin * Phi) * G_psi_spike) * dPhi
+
+    return dvel, dpsi
+
+
+def dPhi_V_of(Phi, V):
+    """Calculating derivative of VPF according to Phi visual angle array at a given timepoint t
+        Args:
+            Phi: linspace numpy array of visual field axis
+            V: binary visual projection field array
+        Returns:
+            dPhi_V: derivative array of V w.r.t Phi
+    """
+    # circular padding for edge cases
+    padV = np.pad(V, (1, 1), 'wrap')
+    dPhi_V_raw = np.diff(padV)
+
+    # we want to include non-zero value if it is on the edge
+    if dPhi_V_raw[0] > 0 and dPhi_V_raw[-1] > 0:
+        dPhi_V_raw = dPhi_V_raw[0:-1]
+
+    else:
+        dPhi_V_raw = dPhi_V_raw[1:, ...]
+
+    dPhi_V = dPhi_V_raw / (Phi[-1] - Phi[-2])
+    return dPhi_V
---
+++ abm/projects/visual_flocking/vf_simulation/vf_sims.py
@@ -0,0 +1,192 @@
+from datetime import datetime
+
+import numpy as np
+import pygame
+
+from abm.agent import supcalc
+from abm.contrib import colors
+from abm.monitoring import ifdb, env_saver
+from abm.projects.visual_flocking.vf_agent.vf_agent import VFAgent
+from abm.simulation.sims import Simulation
+
+
+class VFSimulation(Simulation):
+    def __init__(self, **kwargs):
+        """
+        Inherited from Simulation class
+        :param phototaxis_theta_step: rotational speed scaling factor during
+        phototaxis
+        :param detection_range: detection range of resource patches (in pixels)
+        :param resource_meter_multiplier: scaling factor of how much resource is
+         extraxted for a detected resource unit
+        :param signalling_cost: cost of signalling in resource units
+        :param probability_of_starting_signaling: probability of starting signaling
+        :param des_velocity_res: desired velocity of resource patch in pixel per
+        timestep
+        :param res_theta_abs: change in orientation will be pulled from uniform
+        -res_theta_abs to res_theta_abs
+        :param agent_signaling_rand_event_update: updating agent's
+         random number for signalling probability in every N simulation time step
+        """
+        super().__init__(**kwargs)
+        self.show_all_stats = False
+
+    def draw_agent_stats(self, font_size=15, spacing=0):
+        """Showing agent information when paused"""
+        if self.show_all_stats:
+            font = pygame.font.Font(None, font_size)
+            for agent in self.agents:
+                status = [
+                    f"ID: {agent.id}",
+                    f"meterval: {agent.meter:.2f}",
+                    f"coll.res.: {agent.collected_r:.2f}",
+                ]
+                for i, stat_i in enumerate(status):
+                    text = font.render(stat_i, True, colors.BLACK)
+                    self.screen.blit(
+                        text,
+                        (agent.position[0] + 2 * agent.radius,
+                         agent.position[1] + 2 * agent.radius + i * (
+                                 font_size + spacing)))
+
+    def add_new_agent(self, id, x, y, orient, with_proove=False,
+                      behave_params=None):
+        """
+        Adding a single new agent into agent sprites
+        """
+        agent_proven = False
+        while not agent_proven:
+            agent = VFAgent(
+                id=id,
+                radius=self.agent_radii,
+                position=(x, y),
+                orientation=orient,
+                env_size=(self.WIDTH, self.HEIGHT),
+                color=colors.BLUE,
+                v_field_res=self.v_field_res,
+                FOV=self.agent_fov,
+                window_pad=self.window_pad,
+                pooling_time=self.pooling_time,
+                pooling_prob=self.pooling_prob,
+                consumption=self.agent_consumption,
+                vision_range=self.vision_range,
+                visual_exclusion=self.visual_exclusion,
+                patchwise_exclusion=self.patchwise_exclusion,
+                behave_params=None
+            )
+            self.agents.add(agent)
+            agent_proven = True
+
+    def create_agents(self):
+        """
+        Creating agents according to how the simulation class was initialized
+        """
+        for i in range(self.N):
+            orient = np.random.uniform(0, 2 * np.pi)
+            dist = np.random.rand() * (
+                    self.HEIGHT / 2 - 2 * self.window_pad - self.agent_radii)
+            x = np.cos(orient) * dist + self.WIDTH / 2
+            y = np.sin(orient) * dist + self.HEIGHT / 2
+            if not self.heterogen_agents:
+                # create agents according to environment variables homogeneously
+                self.add_new_agent(i, x, y, orient)
+            else:
+                self.add_new_agent(
+                    i, x, y, orient,
+                    behave_params=self.agent_behave_param_list[i])
+
+    def start(self):
+        start_time = datetime.now()
+        print(f"Running simulation start method!")
+        # Creating N agents in the environment
+        print("Creating agents!")
+        self.create_agents()
+        # Creating surface to show visual fields
+        print("Creating visual field graph!")
+        self.stats, self.stats_pos = self.create_vis_field_graph()
+        # local var to decide when to show visual fields
+        turned_on_vfield = 0
+        print("Starting main simulation loop!")
+        # Main Simulation loop until dedicated simulation time
+        while self.t < self.T:
+            events = pygame.event.get()
+            # Carry out interaction according to user activity
+            self.interact_with_event(events)
+            # deciding if vis field needs to be shown in this timestep
+            turned_on_vfield = self.decide_on_vis_field_visibility(
+                turned_on_vfield)
+
+            if not self.is_paused:
+                # Update agents according to current visible obstacles
+                self.agents.update(self.agents)
+                # move to next simulation timestep (only when not paused)
+                self.t += 1
+            # Simulation is paused
+            else:
+                # Still calculating visual fields
+                for ag in self.agents:
+                    # DEBUG: updating visual projections also when paused
+                    ag.calc_soc_v_proj(self.agents)
+
+            # Draw environment and agents
+            if self.with_visualization:
+                self.draw_frame(self.stats, self.stats_pos)
+                pygame.display.flip()
+            # Monitoring with IFDB (also when paused)
+            if self.save_in_ifd:
+                ifdb.save_agent_data(
+                    self.ifdb_client, self.agents, self.t,
+                    exp_hash=self.ifdb_hash,
+                    batch_size=self.write_batch_size)
+                ifdb.save_resource_data(
+                    self.ifdb_client, self.rescources, self.t,
+                    exp_hash=self.ifdb_hash,
+                    batch_size=self.write_batch_size)
+            elif self.save_in_ram:
+                # saving data in ram for data processing, only when not paused
+                if not self.is_paused:
+                    ifdb.save_agent_data_RAM(self.agents, self.t)
+                    ifdb.save_resource_data_RAM(self.rescources, self.t)
+            # Moving time forward
+            if self.t % 500 == 0 or self.t == 1:
+                print(
+                    f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S.%f')}"
+                    f" t={self.t}")
+                print(f"Simulation FPS: {self.clock.get_fps()}")
+            self.clock.tick(self.framerate)
+        end_time = datetime.now()
+        print(
+            f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S.%f')}"
+            f" Total simulation time: ",
+            (end_time - start_time).total_seconds())
+        # Saving data from IFDB when simulation time is over
+        if self.agent_behave_param_list is not None:
+            if self.agent_behave_param_list[0].get(
+                    "evo_summary_path") is not None:
+                pop_num = self.generate_evo_summary()
+        else:
+            pop_num = None
+        if self.save_csv_files:
+            if self.save_in_ifd or self.save_in_ram:
+                ifdb.save_ifdb_as_csv(exp_hash=self.ifdb_hash,
+                                      use_ram=self.save_in_ram,
+                                      as_zar=self.use_zarr,
+                                      save_extracted_vfield=False,
+                                      pop_num=pop_num)
+                env_saver.save_env_vars([self.env_path], "env_params.json",
+                                        pop_num=pop_num)
+            else:
+                raise Exception(
+                    "Tried to save simulation data as csv file due to env "
+                    "configuration, "
+                    "but IFDB/RAM logging was turned off. Nothing to save! "
+                    "Please turn on IFDB/RAM logging"
+                    " or turn off CSV saving feature.")
+        end_save_time = datetime.now()
+        print(
+            f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S.%f')} "
+            f"Total saving time:",
+            (end_save_time - end_time).total_seconds())
+
+        pygame.quit()
+        # sys.exit()

7ea5304c55adcd3e941bc374b422d1594d901419
ferielamira <ferielamira1@gmail.com>
2024-05-06 20:02:38 +0100

    "Experiment M6 with DDQN for N3,5,8,10 "

--- abm/data/metaprotocol/experiments/MADRLExp0N5R3M6.py
+++ abm/data/metaprotocol/experiments/MADRLExp0N5R3M6.py
@@ -41,7 +41,7 @@ criteria_exp = [
     Constant("TAU", 0.01),
     Constant("OPTIMIZER", "RMSPROP"),
     Constant("PRETRAINED_MODELS_DIR", ""),
-    Constant("BRAIN_TYPE", "DQN"),
+    Constant("BRAIN_TYPE", "DDQN"),
     #Constant("ISE_W", 1.0),
     #Constant("CSE_W", 0.0),
     #Constant("TP", 0),

6cb83f36cefdacaf2eb5dc47dc064713fffd4024
ferielamira <ferielamira1@gmail.com>
2024-05-20 19:32:41 +0100

    "removing enforce exploitatio"

--- abm/projects/madrl_foraging/madrl_agent/brain.py
+++ abm/projects/madrl_foraging/madrl_agent/brain.py
@@ -130,8 +130,7 @@ class DQNAgent:
             if len(self.legal_actions)==1:
 
                 self.action_tensor = torch.LongTensor([[0]]).to(device)
-            if 1 in self.legal_actions:
-                self.action_tensor = torch.LongTensor([[1]]).to(device)
+
 
             else:
                 # Epsilon-greedy exploration
